5648b38acd58e16f7f6872604477486200af7454
Arcady555 <parfenov7233@gmail.com>
2023-01-25 10:00:03 +0300

    Исправления в POM и README

--- src/main/java/ru/job4j/urlshortcut/configuration/WebSecurityConfiguration.java
+++ src/main/java/ru/job4j/urlshortcut/configuration/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package ru.job4j.urlshortcut.websecurity;
+package ru.job4j.urlshortcut.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
